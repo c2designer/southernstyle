df5de71a67a387a503af4499f5e57f3c5bf72514
@c2designer! <cedmonds1@my.apsu.edu>
2019-04-27 16:19:24 -0500

    added layouts, background images, and java classes

--- app/src/main/java/com/example/southernstyle/ServicesActivity.java
+++ app/src/main/java/com/example/southernstyle/ServicesActivity.java
@@ -1,14 +1,12 @@
 package com.example.southernstyle;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
-
+public class ServicesActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_services);
     }
 }
